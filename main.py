39a8a794dde6d627af4a579085041520d4a09908
emilybruns <emilybruns@gmail.com>
2020-10-23 12:05:31 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -16,13 +16,19 @@ def get_fact():
 
     return facts[0].getText()
 
+def get_latinized(fact_input):
+    url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    response = requests.post(url,fact_input)
+    return response.url
+
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
+    fact = get_fact().strip()
+    fact_input = {'input_text': fact}
+    latinized_url = get_latinized(fact_input)
+    return render_template('index.jinja2', url = latinized_url)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
